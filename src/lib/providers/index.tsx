e9f899d8ee4529c6746515062fb07bfdefd5f0c5
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-10-01 17:19:03 +0600

    custom form wrapper created

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/system'
-import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
+import { useRouter } from 'next/navigation'
 
 export interface ProvidersProps {
   children: React.ReactNode
