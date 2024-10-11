eaf2a9486fb361e43971c8ef8c158765cb3f9f8a
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-11 15:54:13 +0600

    create post modal modified

--- src/app/(dashboard)/(user)/dashboard/my-content/page.tsx
+++ src/app/(dashboard)/(user)/dashboard/my-content/page.tsx
@@ -1,9 +1,14 @@
+'use client'
 import CreatePostModal from '@/src/components/modal/CreatePostModal'
 import MyPost from '@/src/components/modules/my-post/MyPost'
 import Container from '@/src/components/ui/Container'
+import { Button } from '@nextui-org/button'
 import { Divider } from '@nextui-org/divider'
+import { useDisclosure } from '@nextui-org/modal'
+import { IoAddCircleOutline } from 'react-icons/io5'
 
 const page = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Container>
       <div className='max-w-3xl w-full mx-auto py-8'>
@@ -11,7 +16,14 @@ const page = () => {
           <h1 className='text-3xl font-bold text-gray-900 dark:text-gray-100'>
             Manage Content
           </h1>
-          <CreatePostModal />
+          <Button
+            color='primary'
+            variant='solid'
+            onPress={onOpen}
+            startContent={<IoAddCircleOutline fontSize={'1.5rem'} />}>
+            Create Post
+          </Button>
+          <CreatePostModal isOpen={isOpen} onClose={onClose} />
         </div>
 
         <Divider className='my-4' />
--- src/components/modal/CreatePostModal.tsx
+++ src/components/modal/CreatePostModal.tsx
@@ -26,6 +26,7 @@ import uploadImageToCloudinary from '@/src/utils/uploadImageToCloudinary'
 import { useGetCurrentUserQuery } from '@/src/redux/features/auth/authApi'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { createPostValidationSchema } from '@/src/schemas/post.schema'
+import { Modal, ModalBody, ModalContent, ModalHeader } from '@nextui-org/modal'
 
 const ReactQuill = dynamic(() => import('react-quill'), {
   ssr: false
@@ -57,6 +58,7 @@ const modules = {
           const file = input?.files?.[0]
 
           if (file) {
+            const toastId = toast.loading('Image uploading', { duration: 2000 })
             if (file.size > 10485760) {
               return toast.warning(
                 'File size exceeds 10 MB limit. Please select a smaller file.'
@@ -65,6 +67,10 @@ const modules = {
             const url = await uploadImageToCloudinary(file)
 
             if (url) {
+              toast.success('Image uploaded successfully', {
+                duration: 2000,
+                id: toastId
+              })
               const quill = (this as any).quill
               const range = quill.getSelection()
 
@@ -106,7 +112,12 @@ export const postCategoriesOptions = [
   { key: 'Road Trips', label: 'Road Trips' }
 ]
 
-const CreatePostModal = () => {
+interface IProps {
+  isOpen: boolean
+  onClose: () => void
+}
+
+const CreatePostModal = ({ isOpen, onClose }: IProps) => {
   const [isPremiumContent, setIsPremiumContent] = useState<boolean>(false)
   const [imageFiles, setImageFiles] = useState<File[] | []>([])
   const [imagePreviews, setImagePreviews] = useState<string[] | []>([])
@@ -150,6 +161,8 @@ const CreatePostModal = () => {
       }
     } catch (error) {
       toast.error('Something went wrong', { duration: 2000 })
+    } finally {
+      onClose()
     }
   }
 
@@ -182,130 +195,145 @@ const CreatePostModal = () => {
   }
 
   return (
-    <TModal
-      buttonText='Create Post'
-      buttonVariant='solid'
-      color={'primary'}
-      icon={<IoAddCircleOutline fontSize={'1.5rem'} />}
+    <Modal
+      backdrop={'blur'}
+      classNames={{
+        base: 'bg-background',
+        header: 'border-b border-divider',
+        footer: 'border-t border-divider',
+        closeButton: 'hover:bg-default-100 active:bg-default-200'
+      }}
+      isOpen={isOpen}
       size='3xl'
-      title='Create a new Post'>
-      {(onClose) => (
-        <TForm
-          resetOnSubmit={true}
-          onSubmit={onSubmit}
-          resolver={zodResolver(createPostValidationSchema)}>
-          <div className='space-y-6'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
-              <div className='sm:col-span-2'>
-                <TInput label='Title' name='title' type='text' />
-              </div>
-              <div>
-                <TSelect
-                  label='Category'
-                  name='category'
-                  options={postCategoriesOptions}
-                  placeholder='Select your post category'
-                />
-              </div>
-              <div>
-                <TInput label='Location' name='location' type='text' />
-              </div>
-              {currentUserData?.data?.isVerified && (
-                <div className='sm:col-span-2 flex items-center'>
-                  <Checkbox
-                    isSelected={isPremiumContent}
-                    radius='full'
-                    value='premium'
-                    onValueChange={setIsPremiumContent}>
-                    <span className='text-sm'>
-                      Make this post premium content
-                    </span>
-                  </Checkbox>
+      placement='center'
+      onOpenChange={onClose}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className='flex flex-col gap-1'>
+              <h2 className='text-xl font-bold'>Edit Comment</h2>
+            </ModalHeader>
+            <ModalBody className='my-8'>
+              <TForm
+                resetOnSubmit={true}
+                onSubmit={onSubmit}
+                resolver={zodResolver(createPostValidationSchema)}>
+                <div className='space-y-6'>
+                  <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+                    <div className='sm:col-span-2'>
+                      <TInput label='Title' name='title' type='text' />
+                    </div>
+                    <div>
+                      <TSelect
+                        label='Category'
+                        name='category'
+                        options={postCategoriesOptions}
+                        placeholder='Select your post category'
+                      />
+                    </div>
+                    <div>
+                      <TInput label='Location' name='location' type='text' />
+                    </div>
+                    {currentUserData?.data?.isVerified && (
+                      <div className='sm:col-span-2 flex items-center'>
+                        <Checkbox
+                          isSelected={isPremiumContent}
+                          radius='full'
+                          value='premium'
+                          onValueChange={setIsPremiumContent}>
+                          <span className='text-sm'>
+                            Make this post premium content
+                          </span>
+                        </Checkbox>
+                      </div>
+                    )}
+                  </div>
+
+                  <div>
+                    <TTextarea label='Short Description' name='description' />
+                  </div>
+
+                  <Divider className='my-4' />
+
+                  <div>
+                    <label
+                      className='block text-sm font-medium text-default-700 mb-2'
+                      htmlFor='image'>
+                      Upload Thumbnail
+                    </label>
+                    <div className='flex items-center justify-center w-full'>
+                      <label
+                        className='flex flex-col items-center justify-center w-full h-32 border-2 border-dashed rounded-lg cursor-pointer bg-default-100 border-default-300 hover:bg-default-200 transition-colors duration-300'
+                        htmlFor='image' // This associates with the input
+                      >
+                        <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                          <FaImage className='w-8 h-8 mb-3 text-default-500' />
+                          <p className='mb-2 text-sm text-default-500'>
+                            <span className='font-semibold'>
+                              Click to upload
+                            </span>{' '}
+                            or drag and drop
+                          </p>
+                          <p className='text-xs text-default-400'>
+                            PNG, JPG, GIF up to 10MB
+                          </p>
+                        </div>
+                      </label>
+                      <input
+                        multiple
+                        accept='image/*'
+                        className='hidden'
+                        id='image'
+                        type='file'
+                        onChange={handleImageChange}
+                      />
+                    </div>
+                  </div>
+
+                  {imagePreviews.length > 0 && (
+                    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-4'>
+                      {imagePreviews.map((imageDataUrl, index) => (
+                        <div key={index} className='relative group'>
+                          <img
+                            alt={`Preview ${index + 1}`}
+                            className='w-full h-32 object-cover rounded-lg'
+                            src={imageDataUrl}
+                          />
+                          <button
+                            className='absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-300'
+                            type='button'
+                            onClick={() => removeImage(index)}>
+                            <FaTrash size={12} />
+                          </button>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+
+                  <ReactQuill
+                    className='mt-3'
+                    formats={formats}
+                    modules={modules}
+                    theme='snow'
+                    value={value}
+                    onChange={setValue}
+                  />
+
+                  <div className='flex justify-center mt-6'>
+                    <Button
+                      className='w-full sm:w-2/3 md:w-1/2 py-2 rounded-lg bg-blue-600 text-white font-semibold transition duration-300 hover:bg-blue-700 mb-6'
+                      size='lg'
+                      type='submit'>
+                      Create Post
+                    </Button>
+                  </div>
                 </div>
-              )}
-            </div>
-
-            <div>
-              <TTextarea label='Short Description' name='description' />
-            </div>
-
-            <Divider className='my-4' />
-
-            <div>
-              <label
-                className='block text-sm font-medium text-default-700 mb-2'
-                htmlFor='image'>
-                Upload Thumbnail
-              </label>
-              <div className='flex items-center justify-center w-full'>
-                <label
-                  className='flex flex-col items-center justify-center w-full h-32 border-2 border-dashed rounded-lg cursor-pointer bg-default-100 border-default-300 hover:bg-default-200 transition-colors duration-300'
-                  htmlFor='image' // This associates with the input
-                >
-                  <div className='flex flex-col items-center justify-center pt-5 pb-6'>
-                    <FaImage className='w-8 h-8 mb-3 text-default-500' />
-                    <p className='mb-2 text-sm text-default-500'>
-                      <span className='font-semibold'>Click to upload</span> or
-                      drag and drop
-                    </p>
-                    <p className='text-xs text-default-400'>
-                      PNG, JPG, GIF up to 10MB
-                    </p>
-                  </div>
-                </label>
-                <input
-                  multiple
-                  accept='image/*'
-                  className='hidden'
-                  id='image'
-                  type='file'
-                  onChange={handleImageChange}
-                />
-              </div>
-            </div>
-
-            {imagePreviews.length > 0 && (
-              <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-4'>
-                {imagePreviews.map((imageDataUrl, index) => (
-                  <div key={index} className='relative group'>
-                    <img
-                      alt={`Preview ${index + 1}`}
-                      className='w-full h-32 object-cover rounded-lg'
-                      src={imageDataUrl}
-                    />
-                    <button
-                      className='absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-300'
-                      type='button'
-                      onClick={() => removeImage(index)}>
-                      <FaTrash size={12} />
-                    </button>
-                  </div>
-                ))}
-              </div>
-            )}
-
-            <ReactQuill
-              className='mt-3'
-              formats={formats}
-              modules={modules}
-              theme='snow'
-              value={value}
-              onChange={setValue}
-            />
-
-            <div className='flex justify-center mt-6'>
-              <Button
-                className='w-full sm:w-2/3 md:w-1/2 py-2 rounded-lg bg-blue-600 text-white font-semibold transition duration-300 hover:bg-blue-700 mb-6'
-                size='lg'
-                type='submit'
-                onPress={() => onClose()}>
-                Create Post
-              </Button>
-            </div>
-          </div>
-        </TForm>
-      )}
-    </TModal>
+              </TForm>
+            </ModalBody>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
   )
 }
 
