1ce91bfc2b78962265325be390d84a8a83713cc0
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-12 19:52:08 +0600

    followers model created

--- src/components/modal/FollowerModal.tsx
+++ src/components/modal/FollowerModal.tsx
@@ -1,6 +1,16 @@
 'use client'
-import { useGetCurrentUserQuery } from '@/src/redux/features/auth/authApi'
+import {
+  useGetCurrentUserQuery,
+  useToggleFollowUnfollowUserMutation
+} from '@/src/redux/features/auth/authApi'
+import { Avatar } from '@nextui-org/avatar'
+import { Button } from '@nextui-org/button'
 import { Modal, ModalBody, ModalContent, ModalHeader } from '@nextui-org/modal'
+import { CheckIcon } from 'lucide-react'
+import { Spinner } from '@nextui-org/spinner'
+import { FiUserCheck, FiUserPlus } from 'react-icons/fi'
+import { Badge } from '@nextui-org/badge'
+import { useState } from 'react'
 
 interface IProps {
   isOpen: boolean
@@ -10,28 +20,108 @@ interface IProps {
 const FollowerModal = ({ isOpen, onClose }: IProps) => {
   const { data: currentUser, isLoading: currentUserLoading } =
     useGetCurrentUserQuery({})
-  console.log('🚀 ~ FollowerModal ~ currentUser:', currentUser)
+
+  const [handleFollow] = useToggleFollowUnfollowUserMutation()
+
+  const [loadingUserId, setLoadingUserId] = useState<string | null>(null)
+
+  const followers = currentUser?.data?.followers
+
+  // handle follow button function
+  const handleFollowToggle = async (id: string) => {
+    if (id) {
+      setLoadingUserId(id)
+      const followData = {
+        followingId: id
+      }
+
+      await handleFollow(followData)
+      setLoadingUserId(null)
+    }
+  }
 
   return (
     <Modal
       backdrop={'blur'}
       classNames={{
-        base: 'bg-background',
+        base: 'bg-background rounded-lg',
         header: 'border-b border-divider',
         footer: 'border-t border-divider',
         closeButton: 'hover:bg-default-100 active:bg-default-200'
       }}
       isOpen={isOpen}
+      scrollBehavior='inside'
       placement='center'
       onOpenChange={onClose}>
       <ModalContent>
         {(onClose) => (
           <>
             <ModalHeader className='flex flex-col gap-1'>
-              <h2 className='text-xl font-bold'>Edit Comment</h2>
+              <h2 className='text-2xl font-bold'>
+                {`${followers?.length} Followers`}
+              </h2>
             </ModalHeader>
-            <ModalBody className='my-8'>
-              <h1>Follwer</h1>
+            <ModalBody className='my-4'>
+              {/* Show spinner if loading */}
+              {currentUserLoading ? (
+                <div className='flex justify-center items-center h-40'>
+                  <Spinner size='lg' color='primary' />
+                </div>
+              ) : followers?.length === 0 ? (
+                <div className='flex justify-center items-center h-40'>
+                  <p className='text-md text-gray-500 font-semibold'>
+                    You are not following anyone yet.
+                  </p>
+                </div>
+              ) : (
+                followers?.map((user) => (
+                  <div
+                    key={user?._id}
+                    className='flex items-center justify-between mb-2 hover:bg-default-100 p-2 rounded-lg transition-all'>
+                    <div className='flex items-center gap-4'>
+                      <div className='relative'>
+                        <Badge
+                          isOneChar
+                          className={`${user?.isVerified ? 'hidden' : ''}`}
+                          color='success'
+                          content={<CheckIcon />}
+                          placement='bottom-right'
+                          shape='circle'>
+                          <Avatar
+                            isBordered
+                            alt={user?.name}
+                            className='w-10 h-10'
+                            color='primary'
+                            src={user?.profileImage}
+                          />
+                        </Badge>
+                      </div>
+                      <p className='font-semibold text-md text-black dark:text-default-800'>
+                        {user?.name}
+                      </p>
+                    </div>
+
+                    {/* follow button */}
+                    <Button
+                      isIconOnly
+                      isLoading={loadingUserId === user?._id}
+                      spinner={<Spinner size='sm' color='white' />}
+                      className={`${
+                        user?.followers?.includes(currentUser?.data?._id)
+                          ? 'bg-success text-white'
+                          : 'bg-primary text-white'
+                      } flex items-center text-sm`}
+                      size='sm'
+                      onClick={() => handleFollowToggle(user?._id)}>
+                      {user?.followers?.includes(currentUser?.data?._id) ? (
+                        <FiUserCheck className='  w-4 h-4' />
+                      ) : (
+                        <FiUserPlus className='  w-4 h-4' />
+                      )}
+                    </Button>
+                  </div>
+                ))
+              )}
             </ModalBody>
           </>
         )}
